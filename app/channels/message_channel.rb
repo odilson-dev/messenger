afebbad7ff2fc9366443acb359347152275da245
odilsonjs <italisodilsonwoodly@hotmail.com>
2024-01-22 11:52:10 -0500

    Implement receive method

--- app/channels/message_channel.rb
+++ app/channels/message_channel.rb
@@ -7,4 +7,8 @@ class MessageChannel < ApplicationCable::Channel
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
   end
+
+  def receive(data)
+    ActionCable.server.broadcast('message', data)
+  end
 end
