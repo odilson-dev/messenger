231b2db87c85d2da490c857056119a4dfab4c117
odilsonjs <italisodilsonwoodly@hotmail.com>
2024-01-21 22:02:40 -0500

    Rebroadcasting

--- app/javascript/channels/message_channel.js
+++ app/javascript/channels/message_channel.js
@@ -1,6 +1,6 @@
 import consumer from "channels/consumer"
 
-consumer.subscriptions.create("MessageChannel", {
+const messageChannel = consumer.subscriptions.create("MessageChannel", {
   connected() {
     // Called when the subscription is ready for use on the server
   },
@@ -13,3 +13,18 @@ consumer.subscriptions.create("MessageChannel", {
     // Called when there's incoming data on the websocket for this channel
   }
 });
+
+document.addEventListener("turbo:load", () => {
+  let form = document.querySelector('#message-form')
+  if(form) {
+    form.addEventListener('submit', (e) => {
+      e.preventDefault()
+      let messageInput = document.querySelector('#message-input').value
+      if(messageInput == '') return;
+      const message = {
+        body: messageInput
+      }
+      messageChannel.send({message: message})
+    })
+  }
+})
