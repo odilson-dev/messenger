a155531c6c668c32b1f9b61627c62a8c85235b72
odilsonjs <italisodilsonwoodly@hotmail.com>
2024-01-22 19:24:25 -0500

    Add create method in message controller

--- app/controllers/hangouts_controller.rb
+++ app/controllers/hangouts_controller.rb
@@ -1,5 +1,6 @@
 class HangoutsController < ApplicationController
   def index
     @message = Message.new
+    @messages = Message.includes(:user)
   end
 end
--- app/javascript/channels/message_channel.js
+++ app/javascript/channels/message_channel.js
@@ -27,18 +27,3 @@ const messageChannel = consumer.subscriptions.create("MessageChannel", {
             </article>`
   }
 });
-
-document.addEventListener("turbo:load", () => {
-  let form = document.querySelector('#message-form')
-  if(form) {
-    form.addEventListener('submit', (e) => {
-      e.preventDefault()
-      let messageInput = document.querySelector('#message-input').value
-      if(messageInput == '') return;
-      const message = {
-        body: messageInput
-      }
-      messageChannel.send({message: message})
-    })
-  }
-})
